af653d07fce18210576adec69953b2061cf6c2f9
git config --global user.name TNorsang <thenorsang@gmail.com>
2018-12-10 22:28:36 -0500

    added style.css game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,20 +13,32 @@ $(document).ready(function() {
         $("#instructions").hide();
     });
 
+    var wins = 0;
+    var losses = 0;
+    var yourScore = 0;
     var minNum = 19;
     var maxNum = 120; 
     var minCrystal = 1;
     var maxCrystal = 12;
 
-    // TODO: random number goes above 120. 
-    var randomNumber = Math.floor(Math.random() * (maxNum + 1) + minNum);  
+
+    // TODO: random number goes above 120. * 6) + 1  
+    var randomNumber = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
     
     $('#targetScore').text(randomNumber);
 
-    $('#crystal-one').val(Math.floor(Math.random() * (maxCrystal + 1) + minCrystal));
-    console.log($('#crystal-one').val())
+    console.log($('#crystal-one').val());
 
-    
+    $('.crystal').click(function() {
+        $('.crystal').val(Math.floor(Math.random() * (maxCrystal - minCrystal + 1)) + minCrystal);
+        console.log($('#crystal-one').val());
+        $('#yourScore').text($('.crystal').val());
+
+    });
+
+    $('#winScore').text(wins);
+    $('#lossScore').text(losses);
+
 });
 
 
