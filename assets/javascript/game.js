5c82fdd8c2de172f8e3309097ea7b491ec623521
git config --global user.name TNorsang <thenorsang@gmail.com>
2018-12-10 23:31:00 -0500

    added audio style.css game.js index.html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,15 @@
 // Waits for the DOM to Load
-$(document).ready(function() {
+$(document).ready(function () {
 
 
 
     $("#instructions").hide();
 
-    $("#how-to-play").click(function() {
+    $("#how-to-play").click(function () {
         $("#instructions").show();
     });
 
-    $("#close").click(function() {
+    $("#close").click(function () {
         $("#instructions").hide();
     });
 
@@ -17,30 +17,52 @@ $(document).ready(function() {
     var losses = 0;
     var yourScore = 0;
     var minNum = 19;
-    var maxNum = 120; 
+    var maxNum = 120;
     var minCrystal = 1;
     var maxCrystal = 12;
 
+    // Generates random Target Score for the user. 
+    var targetScore = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
-    // TODO: random number goes above 120. * 6) + 1  
-    var randomNumber = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-    
-    $('#targetScore').text(randomNumber);
+    $('#targetScore').text(targetScore);
     $('#yourScore').text(yourScore);
 
+    function reset() {
+        yourScore = 0;
+        $('#yourScore').text(yourScore);
+        targetScore = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+        $('#targetScore').text(targetScore);
+    }
 
-    $('.crystal').click(function() {
+    $('.crystal').click(function () {
         $('.crystal').val(Math.floor(Math.random() * (maxCrystal - minCrystal + 1)) + minCrystal);
         yourScore = yourScore + parseInt($('.crystal').val());
         $('#yourScore').text(yourScore);
         console.log(yourScore);
+        
+        
+        if (targetScore === yourScore) {
+            wins++;
+            $('#winScore').text(wins);
+            alert("You Win!");
+            reset();
 
+        } else if (targetScore < yourScore) {
+            losses++;
+            $('#lossScore').text(losses);
+            alert("You Lost! Try Again :)");
+            reset();
+        } 
     });
-
-    $('#winScore').text(wins);
-    $('#lossScore').text(losses);
+$('#winScore').text(wins);
+$('#lossScore').text(losses);
 
 });
+
+
+
+
+
 
 
 
